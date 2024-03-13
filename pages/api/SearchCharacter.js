6b08bb4eb492b5fa732b21aceae2584822b2fb25
LeeSeongDo <dltjdgns3z@naver.com>
2024-03-13 23:47:06 +0900

    Day0313(3)

---
+++ pages/api/SearchCharacter.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+//  캐릭터 검색 API 입니다.
+export  const SearchCharacter = async (Nickname) => {
+    try {
+        const response = await axios.get(`https://developer-lostark.game.onstove.com/characters/${Nickname}/siblings`, {
+            headers: {
+                Authorization: `bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDAwMDExODYifQ.dp5Rwt6qAxGWBF6L00JpgQ8FRk0LC2McvjnYrcIdaVmlW1lcMOhWfDEuQ3d8PBB_bUevh03dw6Shx3sc8_X_B_cUja3eONQ0MWPPa9ZRvHYBjaBn4RPl4pe_M5quBOaQVhTBhcxNYJoCxVQhHfwf_0K0rmAEDHYdSICEIpeD-Ve8WaEBm7JXa36RBP-vefRtcIZh1O35knWa4bXCjuT4rodTYx4WiE_bt4sCUGfaPfzriAe6P5OjlkGx1YEkk3nYGJCVX-cfdIA5qPAc7612BrjV_YuXx5Qh8XzsPL6m5N9v-h-_GAEW10OWSYvxJabPYV8KhPMKanaEpdrpS6i6jA`
+            }
+        });
+        console.log(response);
+        alert("성공");
+    } catch (error) {
+        console.error(error);
+    }
+}
